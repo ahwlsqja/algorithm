b83ea7a80b4500ef225347ae4ab162ac9336da0d
ahwlsqja <100296444+ahwlsqja@users.noreply.github.com>
2024-01-11 09:29:25 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    let arr = [];
+    arr = s.split(" ");
+    for(let i =0 ;i <arr.length; i++)
+        {
+            for(let j = 0; j < arr[i].length; j++)
+                {
+                    if(arr[i].split("")[j])
+                }
+        }
+    return answer;
+}
