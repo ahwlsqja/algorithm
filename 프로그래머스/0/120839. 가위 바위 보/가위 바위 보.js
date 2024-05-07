90fef66ebceeaadcb0bb18f02a5bd715ab3fd745
ahwlsqja <100296444+ahwlsqja@users.noreply.github.com>
2024-05-07 19:38:32 +0900

    [level 0] Title: 가위 바위 보, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,15 @@
+function solution(rsp) {
+    let result = [];
+    for(let i =0; i <rsp.length; i++){
+    if(rsp[i] === "0"){
+        result.push(5)
+    }
+    else if(rsp[i] === "2"){
+        result.push(0)
+    }
+    else {
+        result.push(2)
+    }
+    }
+    return result.join("")
+}
