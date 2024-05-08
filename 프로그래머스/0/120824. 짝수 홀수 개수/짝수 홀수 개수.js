6ec97ecfd0c9fa25a0a880b088884803f1fa4c2a
ahwlsqja <100296444+ahwlsqja@users.noreply.github.com>
2024-05-08 17:00:51 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+    var answer = [];
+    let count1 = 0;
+    let count2 = 0;
+    for(let i =0; i < num_list.length; i++){
+        if(num_list[i] % 2 === 0){
+            count1 +=1;
+        }
+        else{
+            count2 +=1;
+        }
+    }
+    answer.push(count1)
+    answer.push(count2)
+    return answer
+    
+}
