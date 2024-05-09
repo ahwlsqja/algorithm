c00f15dc328a99a2c13bad86f744f1612763a47f
ahwlsqja <100296444+ahwlsqja@users.noreply.github.com>
2024-05-09 21:24:05 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    let sum = 0;
+    for(let i =0; i < my_string.length; i++){
+        if(!isNaN(my_string[i]) && my_string[i] !== ' '){
+            sum += parseInt(my_string[i]);
+        }
+    }
+    return sum;
+}
