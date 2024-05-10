61685404aaef37ac463e566323f0634a25b22e01
ahwlsqja <100296444+ahwlsqja@users.noreply.github.com>
2024-05-10 23:53:17 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    const vowels = 'aeiou';
+    let result = '';
+    for(let i=0; i < my_string.length; i++){
+        if(!vowels.includes(my_string[i])) {
+            result += my_string[i];
+        }
+    }
+    return result;
+}
