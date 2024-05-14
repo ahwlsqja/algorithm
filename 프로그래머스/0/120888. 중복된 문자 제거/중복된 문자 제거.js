189e0647ee38a0660750169838d0e1cee22d4585
ahwlsqja <100296444+ahwlsqja@users.noreply.github.com>
2024-05-14 17:00:58 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    let result =''
+    for(let char of my_string){
+        if(!result.includes(char))
+            {
+                result += char;
+            }
+    }
+    return result
+}
