b8eedd9892be71e12aeb5db228c218c2b5aa7e31
ahwlsqja <100296444+ahwlsqja@users.noreply.github.com>
2024-08-09 13:47:25 +0900

    [level 0] Title: 카운트 다운, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -0,0 +1,8 @@
+function solution(start_num, end_num) {
+    let result = []
+    for(let i = start_num; i > end_num - 1; i--)
+        {
+            result.push(i)
+        }
+    return result;
+}
