0bceb94049990d6a49e03058aaf8368955c7efa0
ahwlsqja <100296444+ahwlsqja@users.noreply.github.com>
2024-08-10 01:36:13 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 16.98 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -0,0 +1,10 @@
+function solution(arr, k) {
+    if(k%2 !== 0)
+        {
+            return arr.map(num => num*k);
+        }
+    else
+        {
+            return arr.map(num => num+k)
+        }
+}
