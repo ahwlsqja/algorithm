a75edfe0774f1fd65c9edaff5f4e32408aa07afc
ahwlsqja <100296444+ahwlsqja@users.noreply.github.com>
2024-08-13 00:19:52 +0900

    [level 0] Title: 배열 만들기 3, Time: 8.10 ms, Memory: 42.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,11 @@
+function solution(arr, intervals) {
+  	let result =  [];
+    for(let i = 0; i < 2; i++)
+        {
+            for(let j = intervals[i][0]; j < intervals[i][1] + 1; j++)
+                {
+                    result.push(arr[j])
+                }
+        }
+    return result;
+}
